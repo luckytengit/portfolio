c32cb386da3bec5b51d833969dd7e5282a22f493
luckytengit <ksh51999@naver.com>
2026-05-28 22:04:41 +0900

    feat: 게시판 Post 부분 개발 - 마이그레이션 파트 작업

--- database/migrations/2026_05_27_061642_create_posts_table.php
+++ database/migrations/2026_05_27_061642_create_posts_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Board\Board;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,18 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignIdFor(User::class)
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignIdFor(Board::class)
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('title');
+            $table->text('content');
+
             $table->timestamps();
         });
     }
