daee2fb2e165d50a3d21cfdb47ca576365d92dd8
luckytengit <ksh51999@naver.com>
2026-06-08 16:39:12 +0900

    Feat: 게시판 Post 부분 개발 - 403 에러 페이지 한글화 작업

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('Forbidden'))
+@section('code', '403')
+@section('message', __($exception->getMessage()=='This action is unauthorized.' ? '권한이 없습니다(로그인 등을 진행해 주십시오)': 'Forbidden'))
