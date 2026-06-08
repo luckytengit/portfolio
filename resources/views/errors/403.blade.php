f8553f763543f0d7bf9c9e0ecc493c101ea0ad35
luckytengit <ksh51999@naver.com>
2026-06-08 16:49:10 +0900

    fix: 게시판 Post 부분 개발 - 403 에러 페이지 구문 관련

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('Forbidden'))
 @section('code', '403')
-@section('message', __($exception->getMessage()=='This action is unauthorized.' ? '권한이 없습니다(로그인 등을 진행해 주십시오)': 'Forbidden'))
+@section('message', __($exception->getMessage()=='This action is unauthorized.' ? '권한이 없습니다(로그인 등을 진행해 주십시오)': ($exception->getMessage() ?: 'Forbidden')))
